5929d61444f8c8a418cce7adfd02faf6be96cc43
Ns <trandinhsinhde@gmail.com>
2019-05-02 10:54:40 +0700

    Add files via upload

---
+++ Guests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ezCloud.Data.Enum
+{
+    public enum Guests
+    {
+        Guest_Name = 1,
+        Guest_Address = 2,
+        Guest_Birthday = 3,
+        Guest_Sex = 4,
+        Guest_Email = 5,
+        Guest_Phone = 6,
+        Guest_Note = 7,
+        Guest_Country = 8
+    }
+}
